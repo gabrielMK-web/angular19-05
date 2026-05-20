d8fb1af9a96d023622508d05819d9f6b0eab0580
joao <joao.martins2704@rede.ulbra.br>
2026-05-19 21:08:39 -0300

    componentes

---
+++ loja-tech/src/app/pages/produto-detalhe/produto-detalhe.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-detalhe',
+  imports: [],
+  templateUrl: './produto-detalhe.html',
+  styleUrl: './produto-detalhe.css',
+})
+export class ProdutoDetalhe {}
