6abbea56a6b748c6d899018928bf3b1dbb3d0e2b
joao <joao.martins2704@rede.ulbra.br>
2026-05-19 21:59:18 -0300

    Atualização vitrine

--- loja-tech/src/app/app.routes.ts
+++ loja-tech/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Vitrine } from './pages/vitrine/vitrine';
+import { ProdutoDetalhe } from './pages/produto-detalhe/produto-detalhe';
+import { Carrinho } from './pages/carrinho/carrinho';
+import { NaoEncontrada } from './pages/nao-encontrada/nao-encontrada';
+
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'vitrine', pathMatch: 'full'
+  },
+
+  {
+    path: 'vitrine',
+    component: Vitrine
+  },
+
+  {
+    path: 'carrinho',
+    component: Carrinho
+  },
+
+  // Rota dinâmica
+  {
+    path: 'produto/:id',
+    component: ProdutoDetalhe
+  },
+
+  // Rota 404
+  {
+    path: '**',
+    component: NaoEncontrada
+  }
+];
--- loja-tech/src/app/pages/vitrine/vitrine.ts
+++ loja-tech/src/app/pages/vitrine/vitrine.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vitrine',
-  imports: [],
-  templateUrl: './vitrine.html',
-  styleUrl: './vitrine.css',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  template: `
+    <h2>Produtos em Destaque</h2>
+
+    <div *ngFor="let prod of produtos">
+      <h4>{{ prod.nome }}</h4>
+      <p>R$ {{ prod.preco }}</p>
+
+      <button [routerLink]="['/produto', prod.id]">
+        Ver Detalhes
+      </button>
+    </div>
+  `
 })
-export class Vitrine {}
+
+export class Vitrine {
+  produtos = [
+    { id: 1, nome: 'Notebook Gamer', preco: 5000 },
+    { id: 2, nome: 'Mouse Sem Fio', preco: 150 },
+    { id: 3, nome: 'Teclado Mecânico', preco: 300 }
+  ];
+}
